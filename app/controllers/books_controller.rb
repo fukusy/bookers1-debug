ddb4b9ea527703c462f5e4111909adcec5fd29db
fukusy <fuku1study@gmail.com>
2021-09-26 03:15:28 +0000

    [Finish]New bookが空の場合バリデーションの表示、editの更新できるよう修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,14 +5,16 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -27,7 +29,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
